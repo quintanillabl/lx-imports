af1f47591398b128613c8f8c1b49065d9d3c5a76
Ruben Cancino <rubencancino6@gmail.com>
2015-08-28 09:13:46 -0500

    Fix

--- grails-app/services/com/luxsoft/impapx/cxp/ComprobanteFiscalService.groovy
+++ grails-app/services/com/luxsoft/impapx/cxp/ComprobanteFiscalService.groovy
@@ -16,101 +16,110 @@ class ComprobanteFiscalService {
 
         def xml = new XmlSlurper().parse(xmlFile)
         SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+        if(xml.name()!='Comprobante')
+            throw new ComprobanteFiscalException(message:"${cfdiFile.getOriginalFilename()} no es un CFDI valido")
 
-        if(xml.name()=='Comprobante'){
-            def data=xml.attributes()
-            log.debug 'Comprobante:  '+xml.attributes()  
+        
+        def data=xml.attributes()
+        log.debug 'Comprobante:  '+xml.attributes()  
 
-            def receptor=xml.breadthFirst().find { it.name() == 'Receptor'}
-            def empresa=Empresa.first()
-            def empresaRfc=Empresa.findByRfc(receptor.attributes()['rfc'])
-            assert empresa.rfc==empresaRfc,"El CFDI ${cfdiFile.getOriginalFilename()} no es corresponde a esta  empresa "
-
-            def emisorNode= xml.breadthFirst().find { it.name() == 'Emisor'}
-
-            def nombre=emisorNode.attributes()['nombre']
-            def rfc=emisorNode.attributes()['rfc']
-            def proveedor=Proveedor.findByRfc(empresa,rfc)
+        def receptor=xml.breadthFirst().find { it.name() == 'Receptor'}
+        def empresa=Empresa.first()
+        def empresaRfc=Empresa.findByRfc(receptor.attributes()['rfc'])
+        if(empresa.rfc!=empresaRfc){
+            throw new ComprobanteFiscalException(message:"El CFDI ${cfdiFile.getOriginalFilename()} no es corresponde a esta  empresa ")
+        }
+        def emisorNode= xml.breadthFirst().find { it.name() == 'Emisor'}
+        def nombre=emisorNode.attributes()['nombre']
+        def rfc=emisorNode.attributes()['rfc']
+        def proveedor=Proveedor.findByRfc(rfc)
             
-            if(!proveedor){
-                log.debug "Alta de proveedor: $nombre ($rfc)"
-                def domicilioFiscal=emisorNode.breadthFirst().find { it.name() == 'DomicilioFiscal'}
-                def dom=domicilioFiscal.attributes()
-                def direccion=new Direccion(
-                    calle:dom.calle,
-                    numeroExterior:dom.noExterior,
-                    numeroInterior:dom.noInterior,
-                    colonia:dom.colonia,
-                    municipio:dom.municipio,
-                    estado:dom.estado,
-                    pais:dom.pais,
-                    codigoPostal:dom.codigoPostal)
-                proveedor=new Proveedor(nombre:nombre,rfc:rfc,direccion:direccion,empresa:empresa)
-                proveedor.save failOnError:true,flush:true
-                
-            }
-            def serie=xml.attributes()['serie']
-            def folio=xml.attributes()['folio']
-            def fecha=df.parse(xml.attributes()['fecha'])
-            def timbre=xml.breadthFirst().find { it.name() == 'TimbreFiscalDigital'}
-            def uuid=timbre.attributes()['UUID']
+        if(!proveedor){
+            log.debug "Alta de proveedor: $nombre ($rfc)"
+            def domicilioFiscal=emisorNode.breadthFirst().find { it.name() == 'DomicilioFiscal'}
+            def dom=domicilioFiscal.attributes()
+            def direccion=new Direccion(
+                calle:dom.calle,
+                numeroExterior:dom.noExterior,
+                numeroInterior:dom.noInterior,
+                colonia:dom.colonia,
+                municipio:dom.municipio,
+                estado:dom.estado,
+                pais:dom.pais,
+                codigoPostal:dom.codigoPostal)
+            proveedor=new Proveedor(nombre:nombre,rfc:rfc,direccion:direccion,empresa:empresa)
+            proveedor.save failOnError:true,flush:true
             
-            def subTotal=data['subTotal'] as BigDecimal
-            def comprobanteFiscal=ComprobanteFiscal.findByUuid(uuid)
-            if(comprobanteFiscal){
-            	throw new RuntimeException("CFDI ${uuid} ya importado");
-            }
-
-        	comprobanteFiscal=new ComprobanteFiscal(
-        		cfdi:xmlFile.getBytes(),
-        		cfdiFileName:cfdiFile.getOriginalFilename(),
-        		uuid:uuid,
-        		serie:serie,
-        		folio:folio
-            )
+        }
+        def serie=xml.attributes()['serie']
+        def folio=xml.attributes()['folio']
+        def fecha=df.parse(xml.attributes()['fecha'])
+        def timbre=xml.breadthFirst().find { it.name() == 'TimbreFiscalDigital'}
+        def uuid=timbre.attributes()['UUID']
             
-            cxp.proveedor=proveedor
-            cxp.documento=folio?:'ND'
-            cxp.fecha=fecha
-            cxp.vencimiento=new Date()+1
-            cxp.moneda=Currency.getInstance('MXN')
-            cxp.tc=1
-            cxp.importe=subTotal
-            cxp.descuentos=0
-            cxp.subTotal=0
-            cxp.impuestos=0
-            cxp.total=0
-            cxp.retTasa=0
-            cxp.retImp=0
-            cxp.comentario="CFDI Importado"
+        def subTotal=data['subTotal'] as BigDecimal
+        def comprobanteFiscal=ComprobanteFiscal.findByUuid(uuid)
+        if(comprobanteFiscal){
+        	throw new RuntimeException("CFDI ${uuid} ya importado");
+        }
 
-            def traslados=xml.breadthFirst().find { it.name() == 'Traslados'}
-            if(traslados){
-                traslados.children().each{ t->
-                    if(t.attributes()['impuesto']=='IVA'){
-                        def tasa=t.attributes()['tasa'] as BigDecimal
-                        cxp.impuestos=t.attributes()['importe'] as BigDecimal
-                    }
+    	comprobanteFiscal=new ComprobanteFiscal(
+    		cfdi:xmlFile.getBytes(),
+    		cfdiFileName:cfdiFile.getOriginalFilename(),
+    		uuid:uuid,
+    		serie:serie,
+    		folio:folio
+        )
+        
+        cxp.proveedor=proveedor
+        cxp.documento=folio?:'ND'
+        cxp.fecha=fecha
+        cxp.vencimiento=new Date()+1
+        cxp.moneda=Currency.getInstance('MXN')
+        cxp.tc=1
+        cxp.importe=subTotal
+        cxp.descuentos=0
+        cxp.subTotal=0
+        cxp.impuestos=0
+        cxp.total=0
+        cxp.retTasa=0
+        cxp.retImp=0
+        cxp.comentario="CFDI Importado"
+
+        def traslados=xml.breadthFirst().find { it.name() == 'Traslados'}
+        if(traslados){
+            traslados.children().each{ t->
+                if(t.attributes()['impuesto']=='IVA'){
+                    def tasa=t.attributes()['tasa'] as BigDecimal
+                    cxp.impuestos=t.attributes()['importe'] as BigDecimal
                 }
             }
-            def retenciones=xml.breadthFirst().find { it.name() == 'Retenciones'}
-            if(retenciones){
-                retenciones.breadthFirst().each{
-                    def map=it.attributes()
-                    if(map.impuesto=='IVA'){
-                       	def imp=map.importe as BigDecimal
-                       	def tasa=imp*100/subTotal
-                       	cxp.retTasa=tasa
-            			cxp.retImp=imp
-                    }
-                    if(map.impuesto=='ISR'){
-                       def imp=map.importe as BigDecimal
-                       def tasa=imp*100/subTotal
-                       cxp.retensionIsr=imp
-                    }
+        }
+        def retenciones=xml.breadthFirst().find { it.name() == 'Retenciones'}
+        if(retenciones){
+            retenciones.breadthFirst().each{
+                def map=it.attributes()
+                if(map.impuesto=='IVA'){
+                   	def imp=map.importe as BigDecimal
+                   	def tasa=imp*100/subTotal
+                   	cxp.retTasa=tasa
+        			cxp.retImp=imp
+                }
+                if(map.impuesto=='ISR'){
+                   def imp=map.importe as BigDecimal
+                   def tasa=imp*100/subTotal
+                   cxp.retensionIsr=imp
                 }
             }
+        }
+        registrarConceptos(cxp,xml)
+        cxp.comprobante=comprobanteFiscal
+        cxp.save failOnError:true,flush:true
+        return cxp
+    }
 
+    def registrarConceptos(def cxp,def xml){
+        if(cxp.instanceOf(FacturaDeGastos)){
             /*
             def conceptos=xml.breadthFirst().find { it.name() == 'Conceptos'}
             conceptos.children().each{
@@ -133,12 +142,12 @@ class ComprobanteFiscalService {
                 gasto.addToPartidas(det)
             }
             */
-                
-            cxp.comprobante=comprobanteFiscal
-            cxp.save failOnError:true,flush:true
-
         }
-        return null
     }
 
+}
+
+class ComprobanteFiscalException extends RuntimeException{
+    String message
+    ComprobanteFiscal comprobante
 }
