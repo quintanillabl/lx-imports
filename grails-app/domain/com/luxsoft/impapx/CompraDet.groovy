085d5f0fde9d78adf44f3a4ee9a1b9f2c2ba9d26
Ruben Cancino <rubencancino6@gmail.com>
2015-09-03 10:33:09 -0500

    entregado dinamico

--- grails-app/domain/com/luxsoft/impapx/CompraDet.groovy
+++ grails-app/domain/com/luxsoft/impapx/CompraDet.groovy
@@ -27,6 +27,11 @@ class CompraDet {
 		descuento(scale:2)
     }
 	static transients =['pendiente']
+
+	static mapping ={
+		entregado formula:'(select ifnull(sum(x.cantidad),0) from embarque_det x where x.compra_det_id=id)'
+		
+	}
 	
 	String toString(){
 		return "${producto?.clave}  ${producto?.descripcion} Com: ${compra?.folio} Pend: ${pendiente} Sol:${solicitado} Rec:${entregado}"
