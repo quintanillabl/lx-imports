ca301d99f6a5829968f83ee5c73fa4757d527088
Ruben Cancino <rubencancino6@gmail.com>
2016-08-06 12:30:54 -0500

    Fix de repositorio

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -45,6 +45,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "http://dl.bintray.com/rcancino/econta" 
     }
 
     dependencies {
